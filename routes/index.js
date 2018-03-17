a75f4551a0c3cd44e69270b3cf9cd47362583242
dpliakos <dimitrispl96@gmail.com>
2018-03-17 20:00:01 +0200

    add routed for past and upcoming events

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.json({"name": "example"});
+  res.json({"OK": "connected"});
 });
 
 module.exports = router;
